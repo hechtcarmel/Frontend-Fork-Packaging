517ec62c1b059741dc64bcd0115c805e205eb12a
CarmelH <hechtcarmel@gmail.com>
2022-03-21 09:26:55 +0200

    fix: Name changes to files and directories

---
+++ reactapp/src/Pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function ErrorPage(){
+    return <div> Wrong Path </div>
+}
+
+export default ErrorPage
--- reactapp/src/Pages/HomePage/HomePage.tsx
+++ reactapp/src/Pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-function Home(){
+function HomePage(){
     return <div> THIS IS HOME PAGE </div>
 }
 
-export default Home
+export default HomePage
