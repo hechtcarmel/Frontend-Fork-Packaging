71d8975856dcff69ba82ccdef12a81e583a3d347
CarmelH <hechtcarmel@gmail.com>
2022-03-21 18:02:51 +0200

    feat: Constants file was added

--- reactapp/electronapp/main.js
+++ reactapp/electronapp/main.js
@@ -3,7 +3,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 require('@electron/remote/main').initialize()
-
+const {REACT_ADDRESS} = require("../src/GeneralConstants");
 
 const fs = require('fs')
 const path = require('path')
@@ -24,13 +24,12 @@ function createWindow () {
             webSecurity: false
         }
     })
-
-
-
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000')
+    console.log(`React Address: ${REACT_ADDRESS}`)
+    mainWindow.loadURL(REACT_ADDRESS)
 
     // Open the DevTools.
+
      mainWindow.webContents.openDevTools()
 }
 
@@ -40,58 +39,22 @@ const { dialog } = require("electron");
 ipcMain.on("alert", (event, data) => {
 // here we can process the data
 // we can send reply to react using below code
-    console.log("AAAAAAAAAA")
+    console.log("electron: ipc alert")
     dialog.showErrorBox("error title", "cool error")
     //event.reply(“send-data-event-name-reply”, ‘Hey react app processed your event’);
 });
-/*
-const {ipcMain, app} = require('electron')
-ipcMain.on('alert', (event, data) => {
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-})
-*/
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     createWindow()
-
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-
-
-
-
-/*
-let mainWindow
-app.on('ready', function(){
-    //create new window
-    mainWindow = new BrowserWindow({})
-
-    console.log(path.join(__dirname,'test.html'))
-
-    //load html into window
-
-
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'test.html'),
-        protocol:'file',
-        slashes: true
-    }))
-})*/
---
+++ reactapp/src/GeneralConstants.js
@@ -0,0 +1,7 @@
+
+const REACT_ADDRESS = 'http://localhost:3000'
+
+
+module.exports = {
+    REACT_ADDRESS
+}
