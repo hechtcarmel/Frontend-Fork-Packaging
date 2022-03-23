2bc1783fde83c1f814ab4db7b9b6b90dd2664792
CarmelH <hechtcarmel@gmail.com>
2022-03-23 23:37:41 +0200

    feat: Searchbar shows only when in the apps page

--- reactapp/src/Pages/Shared/NavigationBar.tsx
+++ reactapp/src/Pages/Shared/NavigationBar.tsx
@@ -3,18 +3,42 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './CSS/NavigationBar.css'
 import { AiOutlineSearch } from 'react-icons/ai';
 import {ChangeEvent, FormEvent, useState} from "react";
+import {useLocation} from "react-router-dom";
+
+
 export default function NavigationBar(){
+    const location = useLocation()
 
     const [searchQuery, setSearchQuery] = useState<string>('')
     const handleSearchSubmit = (event: FormEvent) => {
         event.preventDefault() //Otherwise refreshes the page
-        //console.log("SearchQuery: ", searchQuery)
-
-        
+        console.log('pathname: ', )
+        console.log('location pathname: ', location.pathname)
 
         setSearchQuery('')
     }
 
+    const renderSearchbar = () => {
+        if(location.pathname === '/'){
+            return (
+                <form className="searchArea d-flex input-group w-auto" onSubmit={handleSearchSubmit} >
+                    <input
+                        type="search"
+                        className="form-control rounded"
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="search-addon"
+                        value={searchQuery}
+                        onChange={(e)=> setSearchQuery(e.target.value)}
+                    />
+                    <button type="submit"><AiOutlineSearch /><i className="fa fa-search"></i></button>
+                </form>
+            )
+        }
+        else{
+            return <></>
+        }
+    }
 
 
 
@@ -23,19 +47,7 @@ export default function NavigationBar(){
            <nav className="navbar sticky-top navbar-light bg-light">
                <div className="container-fluid">
                    <a className="navbar-brand">dAppstore</a>
-                   <form className="searchArea d-flex input-group w-auto" onSubmit={handleSearchSubmit} >
-                       <input
-                           type="search"
-                           className="form-control rounded"
-                           placeholder="Search"
-                           aria-label="Search"
-                           aria-describedby="search-addon"
-                           value={searchQuery}
-                           onChange={(e)=> setSearchQuery(e.target.value)}
-                       />
-                       <button type="submit"><AiOutlineSearch /><i className="fa fa-search"></i></button>
-
-                   </form>
+                   {renderSearchbar()}
                </div>
            </nav>
        </>
