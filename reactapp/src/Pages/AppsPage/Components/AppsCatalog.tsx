a58c89ed8fecda2c381a59daa128bea01555a203
CarmelH <hechtcarmel@gmail.com>
2022-03-24 12:13:35 +0200

    minor: installed webstorm plugin libraries

--- reactapp/src/Pages/AppsPage/Components/AppsCatalog.tsx
+++ reactapp/src/Pages/AppsPage/Components/AppsCatalog.tsx
@@ -1,26 +1,20 @@
-
-
-import AppTile from './AppTile'
-import './CSS/AppsCatalogPage.css';
-import {Dispatch, SetStateAction} from "react";
+import AppTile from "./AppTile";
+import "./CSS/AppsCatalogPage.css";
+import { Dispatch, SetStateAction } from "react";
 import AppData from "../AppData";
-import Pagination from "./Pagination"
-interface AppsCatalogProps{
-    displayedApps: AppData[],
-    setDisplayedApps:  Dispatch<SetStateAction<AppData[]>>
-
-
+import Pagination from "./Pagination";
+interface AppsCatalogProps {
+  displayedApps: AppData[];
+  setDisplayedApps: Dispatch<SetStateAction<AppData[]>>;
 }
 function AppsCatalog(props: AppsCatalogProps) {
-
-
-    return (
-            <>
-                {props.displayedApps.map((appData) => (
-                    <AppTile key={appData.id} appData={appData}/>
-                ))}
-            </>
-    );
+  return (
+    <>
+      {props.displayedApps.map((appData) => (
+        <AppTile key={appData.id} appData={appData} />
+      ))}
+    </>
+  );
 }
 
-export default AppsCatalog
+export default AppsCatalog;
